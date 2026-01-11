3d96f42241ae47c7c2b019a29deceefa5eaadb86
Ar-jay <arjayurbina43@gmail.com>
2026-01-11 23:49:19 +0800

    add guide

--- app/src/main/java/com/m/freemovie/Fragment/HomeFragment.java
+++ app/src/main/java/com/m/freemovie/Fragment/HomeFragment.java
@@ -8,6 +8,11 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.app.hubert.guide.NewbieGuide;
+import com.app.hubert.guide.core.Controller;
+import com.app.hubert.guide.listener.OnGuideChangedListener;
+import com.app.hubert.guide.model.GuidePage;
+import com.app.hubert.guide.model.HighLight;
 import com.m.freemovie.R;
 import com.m.freemovie.Retrofit.AppConstant;
 import com.m.freemovie.Utils.SPUtils;
@@ -41,7 +46,36 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         binding.tvMovies.setTextColor(getResources().getColor(R.color.SecondColor));
         binding.tvSeries.setTextColor(getResources().getColor(R.color.white));
         binding.tvTagalog.setTextColor(getResources().getColor(R.color.white));
+        initGuide();
         return binding.getRoot();
+    }
+
+    private void initGuide() {
+        NewbieGuide.with(getActivity())
+                .setLabel("home_guide")
+                .setOnGuideChangedListener(new OnGuideChangedListener() {
+                    @Override
+                    public void onShowed(Controller controller) {
+
+                    }
+
+                    @Override
+                    public void onRemoved(Controller controller) {
+                    }
+                })
+                .addGuidePage(GuidePage.newInstance()
+                        .addHighLight(binding.tvMovies, HighLight.Shape.ROUND_RECTANGLE, 1)
+                        .setLayoutRes(R.layout.guide_movie)
+                )
+                .addGuidePage(GuidePage.newInstance()
+                        .addHighLight(binding.tvSeries, HighLight.Shape.ROUND_RECTANGLE, 1)
+                        .setLayoutRes(R.layout.guide_series)
+                )
+                .addGuidePage(GuidePage.newInstance()
+                        .addHighLight(binding.tvTagalog, HighLight.Shape.ROUND_RECTANGLE, 1)
+                        .setLayoutRes(R.layout.guide_tagalog)
+                )
+                .show();
     }
 
     @Override
