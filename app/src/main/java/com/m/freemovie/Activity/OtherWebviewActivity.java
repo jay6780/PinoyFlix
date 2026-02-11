1645d8e1b7cc89212514cba61e5ffde7d31c5bc3
Ar-jay <arjayurbina43@gmail.com>
2026-02-11 18:29:56 +0800

    other volume

--- app/src/main/java/com/m/freemovie/Activity/OtherWebviewActivity.java
+++ app/src/main/java/com/m/freemovie/Activity/OtherWebviewActivity.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.media.AudioManager;
+import android.media.audiofx.LoudnessEnhancer;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Build;
@@ -12,6 +14,7 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Looper;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.ConsoleMessage;
 import android.webkit.WebChromeClient;
@@ -73,6 +76,12 @@ public class OtherWebviewActivity extends AppCompatActivity
     private RelativeLayout.LayoutParams params,params1;
     private AdView adView;
     private boolean isRotate = false;
+    private LoudnessEnhancer booster;
+    private final int[] gainValues = {-3000, -2000, -1000, 0, 1000, 2000};
+    private final String[] labels = {"0%", "25%", "50%", "100%", "150%", "200%"};
+    private int currentLevelIndex = 3;
+    private CountDownTimer volumeTimer;
+    private AudioManager audioManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -118,6 +127,15 @@ public class OtherWebviewActivity extends AppCompatActivity
         }else{
             loadAdsFailed();
         }
+
+        try {
+            booster = new LoudnessEnhancer(0);
+            booster.setEnabled(true);
+            booster.setTargetGain(-1000);
+            audioManager = (android.media.AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void reset(){
@@ -143,6 +161,50 @@ public class OtherWebviewActivity extends AppCompatActivity
         binding.webView.loadUrl(videoUrl);
 
     }
+
+    private void updateVolume(int index) {
+        if (index < 0 || index >= gainValues.length) return;
+
+        currentLevelIndex = index;
+        booster.setTargetGain(gainValues[index]);
+        int maxSystemVolume = audioManager.getStreamMaxVolume(android.media.AudioManager.STREAM_MUSIC);
+        int targetSystemVol = (index * maxSystemVolume) / 5;
+        audioManager.setStreamVolume(android.media.AudioManager.STREAM_MUSIC, targetSystemVol, 0);
+        binding.volumeSeekBar.setProgress(index);
+        binding.volumeText.setText(labels[index]);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (booster == null) return super.onKeyDown(keyCode, event);
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                binding.llVolume.setVisibility(View.VISIBLE);
+                if (currentLevelIndex < 5) updateVolume(currentLevelIndex + 1);
+                showVolumeUI();
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                binding.llVolume.setVisibility(View.VISIBLE);
+                if (currentLevelIndex > 0) updateVolume(currentLevelIndex - 1);
+                showVolumeUI();
+                return true;
+            default:
+                return super.onKeyDown(keyCode, event);
+        }
+    }
+
+    private void showVolumeUI() {
+        binding.llVolume.setVisibility(View.VISIBLE);
+        if (volumeTimer != null) volumeTimer.cancel();
+
+        volumeTimer = new CountDownTimer(3500, 1000) {
+            public void onTick(long millisUntilFinished) {}
+            public void onFinish() {
+                binding.llVolume.setVisibility(View.GONE);
+            }
+        }.start();
+    }
+
 
     private class CustomWebChromeClient extends WebChromeClient {
         @Override
